c113e672f7a67668bbd4424d8da52d91bae70317
Victor Vicente <victorvpenteadog@gmail.com>
2024-03-11 19:09:27 -0400

    Tirei use client das pages

--- app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
+++ app/Rotas/Financeiro/Comissoes/Detalhes/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Detalhescomissao from "@/components/DetalhesComissao";
 
 export default function PageDetalhesComissao() {
--- app/Rotas/Gestao/Clientes/[Cliente]/page.tsx
+++ app/Rotas/Gestao/Clientes/[Cliente]/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import GestaoCliente from "@/components/GestaoCliente"
 
 
